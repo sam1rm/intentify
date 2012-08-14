cf6a0cadda17a23fcc0c849d21fc36798a0e08f7
Samir Makhani <makhani@berkeley.edu>
2012-08-14 14:11:59 -0400

    changed to chrome extension repo

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Intentify::Application.config.secret_token = '507434fad7cff813d8a22a3d5cd9d13ea2fca3669ae4b478b991a53cf89360043c2d410fffbf72278628f014b2a18943674e9b76098a1b433d6b2a68de8e4930'
